49b7475fa0aa4488980e43bc199caa9dc65cdf9b
Dmitry Lysak <lysak.d@gmail.com>
2023-03-05 19:24:50 +0300

    added 47

---
+++ 47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+using System;
+using static System.Console;
+
+Clear();
+
+double[,] array = GetArray(3, 4);
+WriteLine();
+PrintArray(array);
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]:f1} ");
+        }
+        WriteLine();
+    }
+}
+
+
